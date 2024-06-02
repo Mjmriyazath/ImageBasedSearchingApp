f4d9da30636590097d1f60acf1a2a635d5dc0b15
mjmriyazath <m.j.m.riyazath@gamil.com>
2024-06-03 01:22:39 +0530

    udate 1.0

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import ImageUpload from './ImageUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pixel Search 1.0</h1>
+        <ImageUpload />
+
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
