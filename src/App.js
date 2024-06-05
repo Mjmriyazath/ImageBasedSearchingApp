85a15516a9556f1ec5a3301f3352e9a2c8803be6
Pavithira Primraj Ravichandra <87083427+Pavithira1385@users.noreply.github.com>
2024-06-05 08:27:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pixel Search</h1>
+    {/* <h1>Pixel Search</h1>*/}
         <ImageUpload />
 
 
